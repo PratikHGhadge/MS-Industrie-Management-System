274f07ca7d5f4d9dde254eba732be9a1ff40fc46
PratikHGhadge <pratikhg.2001@gamil.com>
2024-02-04 22:00:04 +0530

    Initial project setup completed

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl flex items-center justify-center m-auto  h-[100vh]  font-bold underline">
+        Hello world
+      </h1>
+    </>
+  );
+}
+
+export default App;
