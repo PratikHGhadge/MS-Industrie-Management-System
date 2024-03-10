6eecf98820c8efe814067a11493066266f9bb740
Rohit <khalaterohit123@gmail.com>
2024-03-10 19:37:04 +0530

    added signup page

--- frontend/src/validations/validationSchemas.tsx
+++ frontend/src/validations/validationSchemas.tsx
@@ -6,6 +6,11 @@ export interface LoginCredentials {
   password: string;
   role: string;
 }
+export interface RegisterCredentials {
+  email?: string;
+  password: string;
+  role: string;
+}
 
 export const LoginValidateYupSchema: Yup.Schema<LoginCredentials> = Yup.object({
   email: Yup.string()
@@ -17,5 +22,20 @@ export const LoginValidateYupSchema: Yup.Schema<LoginCredentials> = Yup.object({
       "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
     )
     .required("Password is Required!"),
+  role: Yup.string().required("role is required"),
+});
+export const RegisterValidateYupSchema: Yup.Schema<RegisterCredentials> = Yup.object({
+  email: Yup.string()
+    .email("Enter the valid email address")
+    .required("email is Required!"),
+  password: Yup.string()
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+    )
+    .required("Password is Required!"),
+  confirmPassword: Yup.string()
+    .required("Password is Required!").
+    oneOf([Yup.ref('password')], "Confirm password must  be same as password"),
   role: Yup.string().required("role is required"),
 });
